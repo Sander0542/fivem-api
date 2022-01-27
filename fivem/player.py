ae7fd4b7d69a5660db0a533096b2f084aa7c700c
Sander Jochems <sanderjochems@hotmail.nl>
2022-01-27 16:29:38 +0100

    Update player.py

--- fivem/player.py
+++ fivem/player.py
@@ -4,22 +4,22 @@ class Player:
         self.name = name
         self.identifiers = Identifiers(identifiers)
         self.ping = ping
-        
+
     @staticmethod
     def parse(data: dict):
         return Player(data.get('id'), data.get('name'), data.get('identifiers'), data.get('ping'))
-        
-        
+
+
 class Identifiers:
     def __init__(self, identifiers):
         self.raw = identifiers
-        
+
         self.license = None
         self.steam = None
         self.xbl = None
         self.discord = None
         self.live = None
-        
+
         if identifiers is not None:
             for identifier in identifiers:
                 service, id = identifier.split(':')
